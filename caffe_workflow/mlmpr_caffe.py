707615e666f818546fe5a3a8ccfe04e03baf0be9
Tomasz Golan <tomasz.golan@gmail.com>
2016-05-13 12:08:06 -0400

    colon to dot (because caffe parser) + beginning of advance command

--- caffe_workflow/mlmpr_caffe.py
+++ caffe_workflow/mlmpr_caffe.py
@@ -5,6 +5,7 @@ Recursive workaround for 2 hours walltime.
 import click
 from ConfigParser import SafeConfigParser
 import os
+from os.path import isfile, join
 from datetime import datetime as date
 import subprocess
 
@@ -18,13 +19,17 @@ def check_paths(paths):
 
 def make_unique(name):
     """Just to make sure there are no name duplicates."""
-    return name + "_" + date.now().isoformat()
+    return name + "_" + date.now().isoformat().replace(":", ".")
 
 
-def generate_pbs(cfg, init=False):
+def generate_pbs(cfg, snapshot=None):
     """Generate PBS script."""
-    pbs = cfg.get("path", "logs") + "/" + cfg.get("status", "name") + ".pbs"
-    out = cfg.get("path", "logs") + "/" + cfg.get("status", "name") + ".out"
+    pbs = cfg.get("path", "logs") + "/" + cfg.get("status", "name") \
+                                  + "_" + cfg.get("status", "current_run") \
+                                  + ".pbs"
+    out = cfg.get("path", "logs") + "/" + cfg.get("status", "name") \
+                                  + "_" + cfg.get("status", "current_run") \
+                                  + ".out"
 
     pbsf = open(pbs, 'w')
 
@@ -33,16 +38,27 @@ def generate_pbs(cfg, init=False):
     print >> pbsf, "#PBS -l nodes=1"
     print >> pbsf, "#PBS -l walltime=2:00:00"
     print >> pbsf, "#PBS -N", cfg.get("status", "name")
-    print >> pbsf, "#PBS -oe", out
+    print >> pbsf, "#PBS -o", out
+    print >> pbsf, "#PBS -j oe"
 
     print >> pbsf, "source $MODULESHOME/init/bash"
     print >> pbsf, "source $PROJWORK/hep105/steven_caffe2/bashsetup.execenv.sh"
     print >> pbsf, "module load cray-mpich"
 
     print >> pbsf, "cd", cfg.get("path", "caffe")
-    print >> pbsf, "aprun -n 1 -N 1", \
-                   "./build/tools/caffe train", \
-                   "--solver=" + cfg.get("status", "solver")
+
+    if not snapshot:
+        print >> pbsf, "aprun -n 1 -N 1", \
+                       "./build/tools/caffe train", \
+                       "--solver=" + cfg.get("status", "solver")
+    else:
+        print >> pbsf, "aprun -n 1 -N 1", \
+                       "./build/tools/caffe train", \
+                       "--solver=" + cfg.get("status", "solver"), \
+                       "--snapshot=" + snapshot
+
+    print >> pbsf, "#python", cfg.get("path", "myself"), "advance --status", \
+        cfg.get("path", "logs") + "/" + cfg.get("status", "name") + ".ini"
 
     pbsf.close()
 
@@ -54,7 +70,7 @@ def generate_solver(cfg):
     solver = cfg.get("path", "logs") + "/" \
                                      + cfg.get("status", "name") + ".solver"
 
-    with open(cfg.get("caffe", "solver")) as f:
+    with open(cfg.get("path", "solver")) as f:
         init_solver = f.readlines()
 
     solverf = open(solver, 'w')
@@ -80,6 +96,25 @@ def submit(pbs):
     subprocess.Popen(cmd, shell=True, executable="/bin/bash")
 
 
+def save_config(cfg):
+    """Save current status."""
+    statusfile = cfg.get("path", "logs") \
+        + "/" + cfg.get("status", "name") + ".ini"
+    with open(statusfile, 'w') as configcopy:
+        cfg.write(configcopy)
+
+
+def get_checkpoint(cfg):
+    """Find the last caffe snapshot."""
+    path = cfg.get("path", "snapshots")
+    prefix = cfg.get("status", "name") + "_iter_"
+    snapshots = [f for f in os.listdir(path) if isfile(join(path, f))]
+    snapshots = [f for f in snapshots if f.startswith(prefix)]
+    snapshots = [f for f in snapshots in f.endswith(".solverstate")]
+    iterations = [i[len(prefix):-len(".solverstate")] for i in snapshots]
+    print iterations
+
+
 @click.group()
 def main():
     pass
@@ -96,19 +131,39 @@ def new(config, name):
     check_paths(cfg.items('path'))
 
     jobname = make_unique(name or "mlmpr_caffe")
+    current_run = 1
+
     cfg.add_section("status")
     cfg.set("status", "name", jobname)
+    cfg.set("status", "current_run", str(current_run))
 
     solver = generate_solver(cfg)
     cfg.set("status", "solver", solver)
 
-    statusfile = cfg.get("path", "logs") + "/" + jobname + ".ini"
-    with open(statusfile, 'wb') as configcopy:
-        cfg.write(configcopy)
+    save_config(cfg)
 
-    pbs = generate_pbs(cfg, True)
+    pbs = generate_pbs(cfg)
 
-    #submit(pbs)
+    submit(pbs)
+
+
+@main.command()
+@click.option('--status', help='status ini file', required=True)
+def advance(status):
+    """Continue job saved with last checkpoint saved in status file."""
+    cfg = SafeConfigParser()
+    cfg.read(status)
+
+    current_run = cfg.getint("status", "current_run") + 1
+    cfg.set("status", "current_run", str(current_run))
+
+    save_config(cfg)
+
+    last_checkpoint = get_checkpoint(cfg)
+
+    pbs = generate_pbs(cfg, last_checkpoint)
+
+    submit(pbs)
 
 if __name__ == '__main__':
     main()
